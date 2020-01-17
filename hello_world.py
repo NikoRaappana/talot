0f39c0b22a81fd4033575a5de3d65accfdd481a2
Mikko <mikko.luhtasaari@tuni.fi>
2020-01-17 13:45:18 +0200

    Add prints

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+print("Hello world!")
+print("Not another hello world")
+print("No")
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("More")
+print("Hello")
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("World")
+print("Please, I'm begging you")
+<<<<<<< HEAD
+
+=======
+>>>>>>> 94c4fbf... Line change
+=======
+print("More")
+>>>>>>> 51905dc... test2
+=======
+>>>>>>> 1662e16... commit
+=======
+print("World")
+>>>>>>> c3cf8d5... qwerty
+=======
+>>>>>>> ca63dde... fix typo
